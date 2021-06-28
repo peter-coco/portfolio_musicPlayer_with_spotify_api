3cfe47930f5d7371a719c5e1e4e1deb638a0e593
coco-peter <dklirser@gmail.com>
2021-06-29 02:11:34 +0900

    deploy test

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -7,9 +7,6 @@ const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=e4ef76d98ff348cfbe2fe41f11d87279&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20playlist-modify-public%20playlist-modify-private";
 
 export default function Login() {
-  // const AUTH_URL = useSelector<GlobalState, string | undefined>(
-  //   (state) => state.authUrl
-  // );
   return (
     <Container
       className="d-flex justify-content-center align-items-center"
